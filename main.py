a1e76fb11463a47fa3068f35fc01b4c60c0fcd70
Niranjan B <niranjannb7777@gmail.com>
2020-12-07 01:38:17 +0530

    Customized Replies

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
+from random import randint
+
 import discord
 from discord.ext import commands
 
+from utils import make_tries, make_scoreboard, super_users, wrong_list, worst_list, correct_list, teams
+
+with open('secret.txt') as file:
+    TOKEN = file.read().strip()
+
 bot = commands.Bot(command_prefix='.')
 
-mark = 30
-qn = 1
-teams = [
-    "arnold",
-    "bergman",
-    "brad",
-    "cameron",
-    "chadwick",
-    "connery",
-    "leonardo",
-    "nolan",
-    "spielberg",
-    "stan",
-    "sylvestor",
-    "tarantino",
-]
-
-super_users = [755703395989585942, 743472398896070657]
-
-
-def make_scoreboard():
-    scoreboard = {}
-    for i in teams:
-        scoreboard[i] = {"points": 0, "attended": []}
-    return scoreboard
-
+tries = make_tries()
 
 scoreboard = make_scoreboard()
 
@@ -37,13 +19,13 @@ answers = {}
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("Let's Informals"))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("Hasta la vista, baby"))
 
 
 @bot.command(name="add")
 async def add(ctx, x, y):
     if ctx.message.author.id not in super_users:
-        await ctx.channel.send("[Error] Super User Command")
+        await ctx.channel.send("```[PERMISSION ERROR] Mrs. Robinson, you're trying to seduce me. Aren't you?```")
         return
     try:
         q = int(x)
@@ -51,7 +33,9 @@ async def add(ctx, x, y):
             answers[q]['valid'].append(y)
         else:
             answers[q] = {"valid": [y, ], "marks": 20}
-        await ctx.channel.send("Answer Added")
+        for i in tries:
+            tries[i]['tries'][q] = 3
+        await ctx.channel.send("```[PASSED] Answer Added```")
     except:
         raise commands.errors.BadArgument
 
@@ -59,16 +43,15 @@ async def add(ctx, x, y):
 @add.error
 async def add_error(ctx, error):
     if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.channel.send("[Error] Missing Arguments")
-        await ctx.channel.send('''```.add <Question Number> <Answer>```''')
+        await ctx.channel.send("```[MISSING ARGS] .add <Question Number> <Answer>```")
     elif isinstance(error, commands.errors.BadArgument):
-        await ctx.channel.send("[Error] Argument Types Don't Match")
+        await ctx.channel.send("```[INVALID ARGS TYPE] int string```")
 
 
 @bot.command(name="clear")
 async def clear(ctx, *args):
     if ctx.message.author.id not in super_users:
-        await ctx.channel.send("[Error] Super User Command")
+        await ctx.channel.send("```[PERMISSION ERROR] Mrs. Robinson, you're trying to seduce me. Aren't you?```")
         return
     global answers
     try:
@@ -83,17 +66,17 @@ async def clear(ctx, *args):
 @clear.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.errors.BadArgument):
-        await ctx.channel.send("[Error] Argument Types Don't Match")
+        await ctx.channel.send("```[INVALID ARGS TYPE] int```")
 
 
 @bot.command(name="mark")
 async def mark(ctx, x, y):
     if ctx.message.author.id not in super_users:
-        await ctx.channel.send("[Error] Super User Command")
+        await ctx.channel.send("```[PERMISSION ERROR] Mrs. Robinson, you're trying to seduce me. Aren't you?```")
         return
     try:
         answers[int(x)]['marks'] = int(y)
-        await ctx.channel.send(f"[Success] Mark Set to {y}")
+        await ctx.channel.send(f"```[PASSED] Mark -> {y}```")
     except:
         raise commands.errors.BadArgument
 
@@ -101,10 +84,9 @@ async def mark(ctx, x, y):
 @mark.error
 async def mark_error(ctx, error):
     if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.channel.send("[Error] Missing Arguments")
-        await ctx.channel.send('''```.mark <Question Number> <Mark>```''')
+        await ctx.channel.send("```[MISSING ARGS] .mark <Question Number> <Mark>```")
     elif isinstance(error, commands.errors.BadArgument):
-        await ctx.channel.send("[Error] Argument Types Don't Match")
+        await ctx.channel.send("```[INVALID ARGS TYPE] int int```")
 
 
 @bot.command(name="answer")
@@ -113,31 +95,39 @@ async def answer(ctx, x, y):
         try:
             q = int(x)
             if q in scoreboard[ctx.channel.name]['attended']:
-                await ctx.channel.send("[Error] Naughty Naughty! You have done it once")
+                r = randint(0, len(worst_list) - 1)
+                await ctx.channel.send(f"```[MULTIPLE ATTEMPTS ERROR] {worst_list[r]}!```")
+                return
+            if tries[ctx.channel.name]['tries'][q] == 0:
+                r = randint(0, len(wrong_list) - 1)
+                await ctx.channel.send(f"```[TRIES EXHAUSTED] {wrong_list[r]}!```")
                 return
             if y in answers[q]['valid']:
                 scoreboard[ctx.channel.name]['attended'].append(q)
                 scoreboard[ctx.channel.name]['points'] += answers[q]['marks']
-                await ctx.channel.send(f"[Success] Kumbayaa Kumbayaa! +{answers[q]['marks']} points")
+                r = randint(0, len(correct_list) - 1)
+                await ctx.channel.send(f"```[CORRECT] {correct_list[r]}! +{answers[q]['marks']} points```")
             else:
-                await ctx.channel.send(f"[Oops] Try Again!")
+                r = randint(0, len(wrong_list) - 1)
+                await ctx.channel.send(f"```[WRONG] {wrong_list[r]}!```")
+            tries[ctx.channel.name]['tries'][q] -= 1
         except:
-            await ctx.channel.send("[Error] Invalid Question Number")
+            r = randint(0, len(wrong_list) - 1)
+            await ctx.channel.send(f"```[INVALID QUESTION] {wrong_list[r]}!```")
 
 
 @answer.error
 async def answer_error(ctx, error):
     if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.channel.send("[Error] Missing Arguments")
-        await ctx.channel.send('''```.answer <Question Number> <Answer>```''')
+        await ctx.channel.send("```[MISSING ARGS] .answer <Question Number> <Answer>```")
     elif isinstance(error, commands.errors.BadArgument):
-        await ctx.channel.send("[Error] Argument Types Don't Match")
+        await ctx.channel.send("```[INVALID ARGS TYPE] int string```")
 
 
 @bot.command(name="all")
 async def all(ctx):
     if ctx.message.author.id not in super_users:
-        await ctx.channel.send("[Error] Super User Command")
+        await ctx.channel.send("```[PERMISSION ERROR] Mrs. Robinson, you're trying to seduce me. Aren't you?```")
         return
     if len(answers) != 0:
         embed = discord.Embed(title=f"__**Answers**__", color=0x03f8fc,
@@ -146,13 +136,13 @@ async def all(ctx):
             embed.add_field(name=i, value=f'>{",".join(answers[i]["valid"])} - {answers[i]["marks"]}', inline=False)
         await ctx.channel.send(embed=embed)
     else:
-        await ctx.channel.send("Empty Answer Set")
+        await ctx.channel.send("```[EMPTY] Answer Set```")
 
 
 @bot.command(name='score')
 async def score(ctx):
     if ctx.message.author.id not in super_users:
-        await ctx.channel.send("[Error] Super User Command")
+        await ctx.channel.send("```[PERMISSION ERROR] Mrs. Robinson, you're trying to seduce me. Aren't you?```")
         return
     embed = discord.Embed(title=f"__**Scoreboard**__", color=0x03f8fc,
                           timestamp=ctx.message.created_at)
@@ -161,4 +151,4 @@ async def score(ctx):
     await ctx.channel.send(embed=embed)
 
 
-bot.run("TOKEN")
+bot.run(TOKEN)
--- utils.py
+++ utils.py
@@ -1,4 +1,38 @@
-from discord.ext.commands.errors import DiscordException
+correct_list = ["Hasta la vista, baby", "Elementary, my dear Watson", "Life will find a way",
+                "We came. We saw. We kicked its ass.", "Kumbayaa Kumbayaa!"]
+wrong_list = ["I pity the fool", "Keep watching the skies!", "Let's go home, Debbie", "Houson, we have a problem",
+              "One, two, Freddy's coming for you ...", "Mama says 'stupid is as stupid does",
+              "Sometimes, you gotta say 'what the beep", "Oh, Moses, Moses, you stubborn, splendid, adorable fool."]
+worst_list = ["You're tearing me apart!", "Open the pod bay doors, HAL.",
+              "Naughty Naughty!"]
 
-class NoPermission(DiscordException):
-        pass
+teams = [
+    "arnold",
+    "bergman",
+    "brad",
+    "cameron",
+    "chadwick",
+    "connery",
+    "leonardo",
+    "nolan",
+    "spielberg",
+    "stan",
+    "sylvestor",
+    "tarantino",
+]
+
+super_users = [755703395989585942, 743472398896070657]
+
+
+def make_tries():
+    tr = {}
+    for i in teams:
+        tr[i] = {'tries': {}}
+    return tr
+
+
+def make_scoreboard():
+    ss = {}
+    for i in teams:
+        ss[i] = {"points": 0, "attended": []}
+    return ss
